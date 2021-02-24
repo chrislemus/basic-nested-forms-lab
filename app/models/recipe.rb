82abd130fa213f416977b053806bc95b6870b28a
chrislemus <icrislemus@gmail.com>
2021-02-24 09:10:22 -0500

    commit tree a0812e77e328c15391cdf03607da5dd34990b73f

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  # accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients)
+    ingredients.each do |ingredient|
+      new_ingredient = Ingredient.find_or_create_by(name: ingredient[:name], quantity: ingredient[:quantity])
+      self.ingredients << new_ingredient
+    end
+    
+  end
 end
