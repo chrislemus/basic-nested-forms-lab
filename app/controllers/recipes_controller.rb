b4b86beb2f30d623c56cbbc8f91ff96ed5e4f21d
chrislemus <icrislemus@gmail.com>
2021-02-24 09:10:27 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,15 @@ class RecipesController < ApplicationController
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipe_params
+  end
+
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
